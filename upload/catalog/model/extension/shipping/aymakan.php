4aa629bd38ed76afecbfd4724b5981c96769b6dc
Abdul Shakoor <shaki_good4u@yahoo.com>
2021-02-28 11:13:51 +0300

    Shipping Model Error Fix

---
+++ upload/catalog/model/extension/shipping/aymakan.php
@@ -0,0 +1,12 @@
+<?php
+class ModelExtensionShippingAymakan extends Model {
+	public function getQuote($address) {
+		$this->load->language('extension/shipping/aymakan');
+
+
+		$method_data = array();
+
+
+		return $method_data;
+	}
+}
